cfade4c83547b38a9ff7d45dc8b8cf164b62962b
Simon Schubert <sschubert89@gmail.com>
2026-02-12 15:29:13 +0100

    Add Kai cask to Casks directory

---
+++ Casks/kai.rb
@@ -0,0 +1,11 @@
+cask "kai" do
+  version "1.6.2"
+  sha256 :no_check
+
+  url "https://github.com/SimonSchubert/Kai/releases/download/v#{version}/Kai-#{version}-macos.dmg"
+  name "Kai"
+  desc "Cross-platform AI chat client with local LLM support"
+  homepage "https:}//github.com/SimonSchubert/Kai"
+
+  app "Kai.app"
+end
