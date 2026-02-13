c15e489c2eddbacb2d5b61b057dbdbdda56a2165
Simon Schubert <sschubert89@gmail.com>
2026-02-13 19:01:53 +0100

    Add Linux Command Library CLI formula

---
+++ linux-command-library-cli.rb
@@ -0,0 +1,21 @@
+class LinuxCommandLibraryCli < Formula
+  desc "Linux command reference CLI tool"
+  homepage "https://github.com/SimonSchubert/LinuxCommandLibrary"
+  version "3.5.10"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/SimonSchubert/LinuxCommandLibrary/releases/download/v#{version}/LinuxCommandLibrary-#{version}-cli-macos-arm64.zip"
+    sha256 "2af75c6617004b911335cca7e46d68d027a4da33c6b6a8258af91bf97fa85609"
+  else
+    url "https://github.com/SimonSchubert/LinuxCommandLibrary/releases/download/v#{version}/LinuxCommandLibrary-#{version}-cli-macos-x64.zip"
+    sha256 "28a7bb6e63edbe925b4f66fe47e187cb9ffa1b27e7703903b663ac067caee70b"
+  end
+
+  def install
+    bin.install "linux-command-library"
+  end
+
+  test do
+    system "#{bin}/linux-command-library", "--version"
+  end
+end
