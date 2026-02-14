3494b6b47e49a2d94393d32f9d5b9874f8927162
Simon Schubert <sschubert89@gmail.com>
2026-02-14 10:44:37 +0100

    Convert braincup from Formula to Cask using DMG release

--- Casks/braincup.rb
+++ Casks/braincup.rb
@@ -1,13 +1,17 @@
-class Braincup < Formula
-  desc "Train your math skills, memory and focus."
+cask "braincup" do
+  version "2.1.0"
+  sha256 "096f9f7b390dd7990d2f418ad30c17efaace50036193bba608ac6ff60393f1da"
+
+  url "https://github.com/SimonSchubert/Braincup/releases/download/v#{version}/Braincup-#{version}-macos.dmg",
+      verified: "github.com/SimonSchubert/Braincup/"
+  name "Braincup"
+  desc "Train your math skills, memory and focus"
   homepage "https://github.com/SimonSchubert/Braincup"
-  url "https://github.com/SimonSchubert/Braincup/raw/master/archive/braincup-1.4.2.zip"
-  sha256 "cde64d7c802d1d657d2e3079f36b68017f12fd9488953b2fb62d54c768153314"
-  version "1.4.2"
 
-  bottle :unneeded
-
-  def install
-    bin.install "braincup"
+  livecheck do
+    url :url
+    strategy :github_latest
   end
+
+  app "Braincup.app"
 end
